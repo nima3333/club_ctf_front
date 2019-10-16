153aae06e988e7f9de8afcce6b765f017eb4aca4
Pierre Gimalac <pierre.gimalac@gmail.com>
2019-10-17 00:15:05 +0200

    Use progress bar in dashboard, more responsive

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -2,40 +2,43 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import React, {Component} from 'react';
 import styles from './Dashboard.module.css';
 import avatar from '../icons/007-hacker-icon.jpg';
+import ProgressBar from 'react-bootstrap/ProgressBar'
 
 class Dashboard extends Component {
   constructor(props) {
     super(props);
-    this.pseudo = "Alice_74";
-    this.avatar = avatar;
-    this.points = 4500;
-    this.rank = 12;
-    this.total_memberf = 12000;
-    this.reussis = 12;
-    this.solutions = 1;
-    this.inventes = 2;
-    this.stats = [
-        {
-          name: "Web",
-          value: 75,
-          color: '#00ff00',
-        },
-        {
-          name: "Crypto",
-          value: 25,
-          color: '#ff0000',
-        },
-        {
-          name: "Forensic",
-          value: 10,
-          color: '#ff0000',
-        },
-        {
-          name: "Stégano",
-          value: 100,
-          color: '#0000ff',
-        }
-    ];
+    this.state = {
+        pseudo: "Alice_74",
+        avatar: avatar,
+        points: 4500,
+        rank: 12,
+        total_memberf: 12000,
+        reussis: 12,
+        solutions: 1,
+        inventes: 2,
+        stats: [
+            {
+              name: "Web",
+              value: 75,
+              color: '#00ff00',
+            },
+            {
+              name: "Crypto",
+              value: 25,
+              color: '#ff0000',
+            },
+            {
+              name: "Forensic",
+              value: 10,
+              color: '#ff0000',
+            },
+            {
+              name: "Stégano",
+              value: 100,
+              color: '#0000ff',
+            }
+        ]
+    };
   }
 
   render() {
@@ -45,30 +48,32 @@ class Dashboard extends Component {
                 <div className={`Dashboard  ${styles.row1}`}>
                     <div className={`Dashboard  ${styles.col_img_info}`}>
                         <div className={`Dashboard  ${styles.img}`}>
-                            <img src={this.avatar} alt="avatar" className={`Dashboard  ${styles.image}`}/>
+                            <img src={this.state.avatar} alt="avatar" className={`Dashboard  ${styles.image}`}/>
                         </div>
                         <div className={`Dashboard  ${styles.col_info}`}>
                             <h6 className={`Dashboard  ${styles.info}`}>
-                                {this.pseudo}
+                                {this.state.pseudo}
                             </h6>
                             <h6 className={`Dashboard  ${styles.info}`}>
-                                {this.points} points
+                                {this.state.points} points
                             </h6>
                             <h6 className={`Dashboard  ${styles.info}`}>
-                                {this.rank} / {this.total_member} membres
+                                {this.state.rank} / {this.state.total_member} membres
                             </h6>
                         </div>
                     </div>
-                    <div className={`Dashboard  ${styles.col1}`}>
-                        <h6 className={`Dashboard  ${styles.info}`}>
-                            {this.reussis} challs réussis
-                        </h6>
-                        <h6 className={`Dashboard  ${styles.info}`}>
-                            {this.solutions} solutions publiées
-                        </h6>
-                        <h6 className={`Dashboard  ${styles.info}`}>
-                            {this.inventes} challs inventés
-                        </h6>
+                    <div className={`Dashboard  ${styles.col_img_info}`}>
+                        <div className={`Dashboard  ${styles.col_right_info}`}>
+                            <h6 className={`Dashboard  ${styles.info_right}`}>
+                                {this.state.reussis} challs réussis
+                            </h6>
+                            <h6 className={`Dashboard  ${styles.info_right}`}>
+                                {this.state.solutions} solutions publiées
+                            </h6>
+                            <h6 className={`Dashboard  ${styles.info_right}`}>
+                                {this.state.inventes} challs inventés
+                            </h6>
+                        </div>
                     </div>
                 </div>
                 <div className={`Dashboard  ${styles.row1}`}>
@@ -78,13 +83,13 @@ class Dashboard extends Component {
                         </span>
                     </div>
                     <div className={`Dashboard  ${styles.col_stat}`}>
-                        {this.stats.map(stat => (
+                        {this.state.stats.map(stat => (
                             <div className={`Dashboard  ${styles.row_stat}`}>
                                 <span className={`Dashboard  ${styles.stat_title}`}>
                                     {stat.name}
                                 </span>
-                                <div className={`Dashboard  ${styles.stat_blank}`}>
-                                    <div className={`Dashboard  ${styles.stat_value}`} style={{width: stat.value + '%', background: stat.color}}/>
+                                <div className={`Dashboard  ${styles.progress_bar}`}>
+                                    <ProgressBar now={stat.value}/>
                                 </div>
                             </div>
                         ))}
